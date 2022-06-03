5a6dac0f436493b2e860378f655ff10e2c2ea5e1
evelynhelena <evelynhelena781@gmail.com>
2022-06-03 15:44:35 -0300

    Realizando autenticação, com possivel erro

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,7 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
+import Router from "next/router";
+import { setCookie, parseCookies, destroyCookie } from 'nookies';
 
 type SignInCredentials = {
     email: string;
@@ -8,6 +10,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
     signIn(cresentials: SignInCredentials): Promise<void>;
+    user: User | undefined,
     isAuthenticated: boolean;
 }
 
@@ -15,12 +18,45 @@ type AuthProviderProps = {
     children: ReactNode
 }
 
+type User = {
+    email: string;
+    permissions: string[];
+    roles: string[];
+}
+
 //diego@rocketseat.team
 
 export const AuthContext = createContext({} as AuthContextData);
 
+ export function signOut() {
+    destroyCookie(undefined, 'nextauth.token');
+    destroyCookie(undefined, 'nextauth.refreshToken');
+
+    Router.push('/');
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
-    const isAuthenticated = false;
+    const [user, setUser] = useState<User | undefined>();
+    const isAuthenticated = !!user;
+
+    useEffect(() => {
+        const { 'nextauth.token': token } = parseCookies();
+
+        if (token) {
+            api.get('/me')
+                .then((response) => {
+                    const { email, permissions, roles } = response.data;
+                    setUser({
+                        email,
+                        permissions,
+                        roles
+                    })
+                }).catch(() => {
+                    signOut();
+                })
+        }
+
+    }, [])
 
     const signIn = async ({ email, password }: SignInCredentials) => {
         try {
@@ -28,15 +64,33 @@ export function AuthProvider({ children }: AuthProviderProps) {
                 email,
                 password
             })
-            console.log(response.data);
+
+            const { token, refreshToken, permissions, roles } = response.data;
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 25 * 30,
+                path: '/'
+            });
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 25 * 30,
+                path: '/'
+            });
+            setUser({
+                email,
+                permissions,
+                roles
+            })
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            Router.push('/dashboard');
         } catch (e) {
-            alert("Erro");
+            alert("Usuário ou senha incorreto");
         }
 
     }
 
     return (
-        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+        <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
             {children}
         </AuthContext.Provider>
     )
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,86 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
+import { signOut } from '../context/AuthContext';
+/* import { signOut } from '../context/AuthContext'; */
+
+interface FailedRequestQueue {
+    onSuccess: (token: string) => void;
+    onFailure: (error: AxiosError) => void;
+};
+
+interface AxiosErrorResponse {
+    code?: string;
+}
+let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue = Array<FailedRequestQueue>();
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'http://localhost:3333',
+    headers: {
+        Authorization: `Bearer ${cookies['nextauth.token']}`,
+    }
 });
+
+api.interceptors.response.use(response => {
+    return response;
+}, (error: AxiosError<AxiosErrorResponse>) => {
+    if (error.response?.status === 401) {
+        if (error.response.data?.code === "token.expired") {
+            //renovar token
+            cookies = parseCookies();
+            const { 'nextauth.refreshToken': refreshToken } = cookies;
+            const originalConfig = error.config
+
+            if (!isRefreshing) {
+                isRefreshing = true;
+
+                api.post('/refresh', { refreshToken }).then(response => {
+                    const { token } = response.data;
+
+                    setCookie(undefined, 'nextauth.token', token, {
+                        maxAge: 60 * 60 * 25 * 30,
+                        path: '/'
+                    });
+                    setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+                        maxAge: 60 * 60 * 25 * 30,
+                        path: '/'
+                    });
+
+                    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+                    failedRequestsQueue.forEach(request => request.onSuccess(token));
+                    failedRequestsQueue = [];
+
+                }).catch(err => {
+                    failedRequestsQueue.forEach(request => request.onFailure(err));
+                    failedRequestsQueue = [];
+                }).finally(() => {
+                    isRefreshing = false;
+                });
+            }
+
+            return new Promise((resolve, reject) => {
+                failedRequestsQueue.push({
+                    onSuccess: (token: string) => {
+                        if (!originalConfig?.headers) {
+                            return
+                        }
+
+                        originalConfig.headers['Authorization'] = `Bearer ${token}`;
+                        resolve(api(originalConfig));
+
+                    },
+                    onFailure: (err: AxiosError) => {
+                        reject(err)
+                    }
+                })
+            });
+
+        } else {
+            console.log("aqui")
+            signOut(); 
+        }
+    }
+   return Promise.reject(error);
+})
